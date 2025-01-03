217ac7548bcf5c41287b0282c9a41f7e9f7731f9
tenyuu <aw.gh.tenyu@gmail.com>
2025-01-03 14:03:35 +0900

    ラジオボタンの追加

--- Window2.py
+++ Window2.py
@@ -1,11 +1,19 @@
 import tkinter
 
 root = tkinter.Tk()
-root.title("Window practice!")
-root.geometry("200x500")
-root.resizable(0,1)
-root.config(bg="red")
+root.title("practice!")
+root.geometry("500x500")
+root.resizable(0,0)
 
+frame_1 = tkinter.LabelFrame(root,text="テキストフレームです")
+frame_2 = tkinter.Frame(root)
+frame_1.pack(padx=10,pady=10)
+frame_2.pack(padx=10,pady=(0,10))
+             
+radio_1 = tkinter.Radiobutton(frame_1,text="1を出力します")
+radio_2 = tkinter.Radiobutton(frame_1,text="2を出力します")
+radio_1.grid(row=0,column=0,padx=10,pady=10)
+radio_2.grid(row=0,column=1,padx=10,pady=10)
 
 
 root.mainloop()
